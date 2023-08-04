3c3163d60f160f15eab6c533f631aa63b62ed8e1
Ajee9895 <ajeethkumar.gopal@gmail.com>
2023-08-04 12:18:34 +0530

    happy commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,24 +5,32 @@ import { auth, provider } from "../../firebase";
 import { useStateValue } from "../ContextApi/StateProvider";
 import { actionTypes } from "../ContextApi/reducer";
 import { signInWithPopup } from "firebase/auth";
+import { GoogleLogin } from 'react-google-login';
 
 const Login = () => {
   const [state, dispatch] = useStateValue();
 
   //to open a pop up sign in with google account
-  const signIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        dispatch({
-          type: actionTypes.SET_USER,
-          user: result.user,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err.message);
-      });
-  };
+  // const signIn = () => {
+  //   signInWithPopup(auth, provider)
+  //     .then((result) => {
+  //       dispatch({
+  //         type: actionTypes.SET_USER,
+  //         user: result.user,
+  //       });
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       alert(err.message);
+  //     });
+  // };
+
+  const responseGoogle = (response) => {
+    dispatch({
+      type: actionTypes.SET_USER,
+      user: response.user,
+    });
+  }
 
   return (
     // Login page
@@ -35,9 +43,16 @@ const Login = () => {
         <div className="login__text">
           <h1>Sign in to WhatsApp</h1>
         </div>
-        <Button type="submit" onClick={signIn}>
+        {/* <Button type="submit" onClick={signIn}>
           Sign In With Google
-        </Button>
+        </Button> */}
+        <GoogleLogin
+          clientId="603709256937-ujep62d7m9g4capksnvmoag2nklq3gj5.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </div>
     </div>
   );
